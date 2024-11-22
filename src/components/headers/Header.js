96711818763e6f4adadadd37d7ac4778d76b8323
naresh <nareshm5796@gmail.com>
2024-11-22 22:37:24 +0530

    header fles error2

--- src/components/headers/Header.js
+++ src/components/headers/Header.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../headers/Header.css'
+import '../headers/header.css'
 import logo from '../assets/logo.png';
 import EastOutlinedIcon from '@mui/icons-material/EastOutlined';
 import SelectDropDown from '../selectdropdown/SelectDropDown'
